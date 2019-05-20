25925e3a029826e47541d72dc1619ff31c125c5a
gao_maolin <gao_maolin@hoperun.com>
2019-05-20 11:01:43 +0800

    修改文件

---
+++ src/test/java/com/example/demo/Test.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: Mr.Gao
+ * @create: 2019-05-17 15:12
+ **/
+public class Test {
+    
+
+    /**
+    * @Title testYYH 
+    * @Description: TODO 
+    * @Param: [name, age] 
+    * @return: java.lang.String 
+    * @Author: Mr.Gao
+    * @Date: 2019/5/17 
+    */ 
+    public String testYYH(String name, int age){
+       
+        return "123";
+    }
+    /**
+    * @Title test 
+    * @Description: TODO 
+    * @Param: [name] 
+    * @return: java.lang.String 
+    * @Author: Mr.Gao
+    * @Date: 2019/5/17 
+    */ 
+    public String test(String name){
+        return "123";
+    }
+
+    /**
+    * @Title testThing
+    * @Description: TODO
+    * @Param: [name, i]
+    * @return: void
+    * @Author: Mr.Gao
+    * @Date: 2019/5/20
+    */
+    public void testThing(String name, int i){
+
+    }
+}
