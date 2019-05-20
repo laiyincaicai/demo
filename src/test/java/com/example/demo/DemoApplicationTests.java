24b9089e31fa9ab503c8ba96a1b4d5cbad08d372
gao_maolin <gao_maolin@hoperun.com>
2019-05-20 10:46:33 +0800

    修改文件

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,18 +7,20 @@ import java.util.List;
 @SpringBootTest*/
 public class DemoApplicationTests {
 
+
     public static void main(String arg[]) {
         List<String> list1 = new ArrayList<>();
         list1.add("1");
-        list1.add("2");
+        list1.add( "2");
         list1.add("3");
         list1.add("4");
         list1.add("5");
         for (String str : list1) {
             System.out.println(str);
         }
-        System.out.println("===================");
-        for (int i = 0; i < list1.size(); i++) {
+           System.out.println("===================");
+        for (  int i = 0; i < list1.size(); i++)
+        {
             if(i == 3){
                 System.out.println(String.valueOf(i).equals(list1.get(i - 1)));
             }
